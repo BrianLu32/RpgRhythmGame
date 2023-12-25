9906380523e2d4a2882272c92d8c79de481b3462
Brian Lu <brian.lu62@gmail.com>
2023-12-24 20:27:54 -0600

    Added Jump and Sprint to Movement

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -8,15 +8,38 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed;
+    readonly KeyCode sprintKey = KeyCode.LeftShift;
+    public float speed = 12f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public float gravity = -19.62f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        // Check if player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        float sprintMultiplier = Input.GetKey(sprintKey) ? 1.5f : 1f;
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if(direction.magnitude >= 0.1) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -24,7 +47,7 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * speed * sprintMultiplier * Time.deltaTime);
         }
     }
 }
