e9d2cfdd99bb3306c9dfe9c0ce4232d510c7d1b8
Brian Lu <brian.lu62@gmail.com>
2024-06-23 22:51:30 -0500

    Added basic export for song timings

--- Assets/Scripts/MapEditor/ExportBeatmap.cs
+++ Assets/Scripts/MapEditor/ExportBeatmap.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ExportBeatmap : MonoBehaviour
 {
+    [SerializeField] private AudioManager audioManager;
     [SerializeField] private GameObject timelineObject;
     private Timeline timelineInstance;
 
@@ -12,10 +14,15 @@ public class ExportBeatmap : MonoBehaviour
     }
 
     public void Export() {
+        // Path for file
+        string path = Application.dataPath + "/SongExports/" + audioManager.song.clip.name + ".txt";
+        StreamWriter writer = File.AppendText(path);
+
         foreach(var marker in timelineInstance.markerSets) {
             if(marker.GetComponent<Marker>().hasTimeStamp) {
-                
+                writer.WriteLine(marker.GetComponent<Marker>().timeStamp + "\n");
             }
         }
+        writer.Close();
     }
 }
